512a2f840b5e8a27ceedf93da187b1e6a0066f4e
pchabbi7 <pri.17feb@gmail.com>
2023-11-16 19:08:26 -0500

    Adding semi working method

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,19 @@
+class Phrase
+
+  def initialize(string)
+      @words_list = string.downcase.scan(/[\w']+/)
+  end
+
+  def word_count
+    counts = Hash.new
+    @words_list.each do |word|
+      if counts.key?(word)
+        counts[word] = counts[word] + 1
+      else
+        counts.store("#{word}", 1)
+      end
+    end
+    counts
+  end
+
+end
