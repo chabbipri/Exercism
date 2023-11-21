3e3a7c4486a1807b50eb597c68ced0ce79f8abfc
pchabbi7 <pri.17feb@gmail.com>
2023-11-21 10:51:36 -0500

    Adding refactored method with tally

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,17 +1,15 @@
 class Phrase
+  attr_reader :words
+
   def initialize(string)
-      @words_list = string.downcase.scan(/[\w']+/)
+    @words = string.downcase.scan(/[\w']+/)
   end
 
   def word_count
-    counts = Hash.new
-    @words_list.each do |word|
-      if counts.key?(word)
-        counts[word] = counts[word] + 1
-      else
-        counts.store("#{word}", 1)
-      end
-    end
-    counts
+    # counts = Hash.new(0)
+    # words.each do |word|
+    #   counts[word] += 1
+    # end
+    words.tally
   end
 end
--- ruby/word-count/word_count_test.rb
+++ ruby/word-count/word_count_test.rb
@@ -1,5 +1,5 @@
-require 'minitest/autorun'
-require_relative 'word_count'
+require "minitest/autorun"
+require_relative "word_count"
 
 class WordCountTest < Minitest::Test
   def test_count_one_word
