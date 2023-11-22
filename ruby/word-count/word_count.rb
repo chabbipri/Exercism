f17cc07039bd8bb8cd4911029613b6f87ef96f31
Priyanka Chabbi <109626533+chabbipri@users.noreply.github.com>
2023-11-21 23:04:57 -0500

    Exercism Ruby: Word count (#2)
    * Adding semi working method
    
    * Adding last working set of code
    
    * Adding refactored method with tally
    
    * Adding the run script to include rubocop
    
    * Adding the working solution of word-count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,14 +1,28 @@
 class Phrase
+  attr_reader :words
 
   def initialize(string)
-    @words_list = string.split(" ")
+    @words = fetch_only_words(string)
   end
 
   def word_count
-    count = Hash.new
-    @words_list.each do |word|
-      count.store("#{word}", 1)
+    # counts = Hash.new(0)
+    # words.each do |word|
+    #   word.gsub!(/\A\W|\W\z/, "")
+    #   counts[word] += 1
+    # end
+    # counts
+    words.tally
+  end
+
+  private
+
+  def fetch_only_words(string)
+    words = string.downcase.scan(/[\w']+/)
+
+    words.map do |word|
+      word.gsub!(/\A\W|\W\z/, "")
     end
-    count
+    words
   end
 end
