ca5a8f33d9528738eb938d36eed11667175e74f9
pchabbi7 <pri.17feb@gmail.com>
2024-03-13 16:58:55 -0400

    Refactored the code, used the .count.

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,13 @@
 class Acronym
-    def self.abbreviate(string)
-        abbreviate = ""
-        string.split(/\W/).map{|word| abbreviate += word[0] if word != ""}
-        abbreviate.upcase
-    end 
+  def self.abbreviate(sentence)
+    words(sentence).map(&:chr).join.upcase
+  end
+
+  private
+
+  def self.words(sentence)
+    sentence.split(/\W/).reject(&:empty?)
+  end
 end 
     
  
--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,24 +1,20 @@
 class Hamming
-
   def self.compute(dna_1, dna_2)
-    argument_error_check(dna_1, dna_2)
-
+    validate_args(dna_1, dna_2)
     return 0 if dna_1 == dna_2
 
-    if dna_1 != dna_2
-      fetch_hamming_distance(dna_1.split(""), dna_2.split(""))
-    end
+    hamming_distance(dna_1.chars, dna_2.chars)
   end
 
   private
 
-  def self.fetch_hamming_distance(dna_1_array, dna_2_array)
-    dna_1_array.zip(dna_2_array).map do
-      |val1, val2| val1 if val1 != val2
-    end.compact.size
+  def self.validate_args(dna_1, dna_2)
+    raise ArgumentError if dna_1.size != dna_2.size
   end
 
-  def self.argument_error_check(dna_1, dna_2)
-    raise ArgumentError if dna_1.size != dna_2.size
+  def self.hamming_distance(dna_1_array, dna_2_array)
+    dna_1_array.zip(dna_2_array).count do
+      |val1, val2| val1 != val2
+    end
   end
 end
