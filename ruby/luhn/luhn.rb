54acc79c767d5d90783b79b5fc1d24be2770208c
pchabbi7 <pri.17feb@gmail.com>
2024-03-24 10:33:59 -0400

    Adding the first iteration of the solution.

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,7 +1,30 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
+require 'pry'
+class Luhn
+  def self.valid?(digits_string)
+    return false if digits_string.chars.size == 1
 
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
+    return false if !digits_string.gsub(" ", "").scan(/\D/).empty?
+
+    digits = digits_string.reverse.gsub(" ", "").chars
+
+    return false if digits.size == 1
+
+    sum = 0
+    i = 1
+
+    digits.each do |digit|
+      i % 2 == 0 ? sum += double_digit(digit) : sum += digit.to_i
+      i += 1
+    end
+    sum % 10 == 0
+  end
+
+  private
+
+  def self.double_digit(digit)
+    double_digit = digit.to_i * 2
+    digit = double_digit - 9 if double_digit > 9
+    digit = double_digit if double_digit <= 9
+    digit.to_i
+  end
+end
