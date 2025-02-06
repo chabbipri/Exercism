be6ca4a2a6cac2f4f41c9d4c157109f036bbea73
pchabbi7 <pri.17feb@gmail.com>
2025-02-06 13:52:01 -0500

    First: Pratice the Solution

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,28 +1,3 @@
 class Luhn
-  def self.valid?(digits_string)
-    string = digits_string.gsub(" ", "")
-    non_digits = string.scan(/\D/)
-    return false if non_digits.any?
 
-    digits = string.reverse.chars.map(&:to_i)
-    return false if digits.one?
-
-    sum =
-      digits.map.with_index do |digit, i|
-        i.odd? ? double_digit(digit) : digit
-      end.sum
-
-    divisible?(sum, by: 10)
-  end
-
-  private
-
-  def self.double_digit(digit)
-    (digit * 2).digits.sum
-  end
-
-  def self.divisible?(dividend, by:)
-    divisor = by
-    dividend % divisor == 0
-  end
 end
