9f8a16f2020f76cbb4d7b1f90dad9c5615800d30
pchabbi7 <pri.17feb@gmail.com>
2025-02-06 15:01:20 -0500

    Redo: 1

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,3 +1,18 @@
 class Luhn
+  def self.valid?(number)
+    return false unless number.gsub(" ", "").scan(/\D/).empty?
 
+    numbers = number.scan(/[0-9]/).map(&:to_i)
+
+    return false if numbers.length < 2
+
+    numbers.reverse!
+
+    numbers.each_with_index do |num, index|
+        if index.odd?
+           numbers[index] = (num * 2).digits.sum
+        end
+    end
+    numbers.sum % 10 == 0
+  end
 end
